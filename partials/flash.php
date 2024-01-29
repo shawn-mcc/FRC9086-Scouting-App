546a649175f1ca639fb73351c10e66ef2e6149c2
Kaleb Snyder <42294@s.bryan.k12.ga.us>
2024-01-29 14:57:20 -0500

    Showing codeowner

--- partials/flash.php
+++ partials/flash.php
@@ -2,6 +2,8 @@
 /*put this at the bottom of the page so any templates
  populate the flash variable and then display at the proper timing*/
 ?>
+
+
 <div class="container" id="flash">
     <?php $messages = getMessages(); ?>
     <?php if ($messages) : ?>
